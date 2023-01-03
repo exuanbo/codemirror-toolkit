a0adf6c56a7b5b14e7e6f34dd839a7318f4346c7
exuanbo <exuanbo@protonmail.com>
2023-01-03 01:43:08 +0000

    refactor(react): use import alias

--- packages/react/src/utils/batch.ts
+++ packages/react/src/utils/batch.ts
@@ -1,8 +1,8 @@
-import { version } from 'react'
+import { version as REACT_VERSION } from 'react'
 import { unstable_batchedUpdates } from 'react-dom'
 
 function noopBatch(callback: () => void) {
   callback()
 }
 
-export const batch = version.startsWith('18') ? noopBatch : unstable_batchedUpdates
+export const batch = REACT_VERSION.startsWith('18') ? noopBatch : unstable_batchedUpdates
