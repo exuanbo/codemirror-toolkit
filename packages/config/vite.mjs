9418ec241fc380384a958b45e03fbfd7f6e2ad44
exuanbo <exuanbo@protonmail.com>
2023-01-08 00:34:08 +0000

    test: use lcov

--- packages/config/vite.mjs
+++ packages/config/vite.mjs
@@ -33,7 +33,7 @@ export function createViteConfig(config) {
       ...config?.test,
       coverage: {
         enabled: true,
-        reporter: ['text', 'html', 'json'],
+        reporter: ['text', 'lcov'],
         ...config?.test?.coverage,
       },
     },
