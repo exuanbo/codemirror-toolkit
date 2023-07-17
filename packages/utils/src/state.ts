c49457139d7f254e651e056edc105babc75afb87
exuanbo <exuanbo@protonmail.com>
2023-07-17 21:44:44 +0100

    feat(utils/state): add overloaded signature

--- packages/utils/src/state.ts
+++ packages/utils/src/state.ts
@@ -4,6 +4,15 @@ export function isEffectOfType<T>(type: StateEffectType<T>) {
   return (effect: StateEffect<unknown>): effect is StateEffect<T> => effect.is(type)
 }
 
-export function mapEffectValue<T, R>(fn: (value: T) => R) {
-  return (effect: StateEffect<T>) => fn(effect.value)
+export function mapEffectValue<T, R>(fn: (value: T) => R): (effect: StateEffect<T>) => R
+
+export function mapEffectValue<T, R>(effect: StateEffect<T>, fn: (value: T) => R): R
+
+export function mapEffectValue<T, R>(
+  arg1: ((value: T) => R) | StateEffect<T>,
+  arg2?: (value: T) => R,
+) {
+  return typeof arg1 === 'function'
+    ? (effect: StateEffect<T>) => arg1(effect.value)
+    : arg2!(arg1.value)
 }
